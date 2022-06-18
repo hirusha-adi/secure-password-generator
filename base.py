2bbb2897f1b928235bd7bd21903463de58269eb6
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-18 15:16:10 +0530

    Update base.py

--- base.py
+++ base.py
@@ -1,18 +1,21 @@
-import time
 import os
-import secrets
 import typing as t
+from random import SystemRandom
+
+
 
 class SecurePassword:
     def __init__(self) -> None:
+        self._sysrand = SystemRandom()
+        
         self._passwd = None
         self._generated_count: int = 1
         
         self._ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
         self._ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         self._digits = '0123456789'
-        self._punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
-        self._all = self._ascii_lowercase + self._ascii_uppercase + self._digits + self._punctuation
+        self._symbols = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+        self._all = self._ascii_lowercase + self._ascii_uppercase + self._digits + self._symbols
     
     def __repr__(self) -> str:
         return f"<Count: {self._generated_count}, LastPassword: {self._passwd}>"
@@ -22,16 +25,30 @@ class SecurePassword:
         length: t.Union[int, None] = None, 
         lower: t.Union[bool, None] = None,
         upper: t.Union[bool, None] = None, 
-        num: t.Union[bool, None] = None, 
+        digits: t.Union[bool, None] = None, 
         symbols: t.Union[bool, None] = None
     ):
         
+        character_set = r""
         if (150 <= length) and (length >= 3):
-            pass 
-            self._generated_count += 1
+            if lower:
+                character_set += self._ascii_lowercase
+            if upper:
+                character_set += self._ascii_uppercase
+            if digits:
+                character_set += self._digits
+            if symbols:
+                character_set += self._digits
 
-        self._passwd = ''.join(secrets.choice(self._all) for i in range(length))  
+        self._passwd = ''.join(
+            self._sysrand.choice(self._all) 
+            for i in range(
+                length
+                )
+            )  
+
+        self._generated_count += 1
 
 obj = SecurePassword()
-obj.password(5)
+obj.password(100)
 print(obj)
